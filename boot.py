738d761dcbf8302ba7c0cdf5ac0ceb29cc231c3d
Mateusz Drab <mateuszd@mpd.pw>
2024-12-09 00:49:56 +0000

    add environment variable to enable/disable auto toggle of the mouse mover on reset

--- boot.py
+++ boot.py
@@ -1,6 +1,7 @@
 import random
 import storage
 import supervisor
+import os
 
 storage.remount("/", readonly=False)
 print("Storage remounted read-write, USB storage will be read-only")
@@ -8,6 +9,7 @@ print("Storage remounted read-write, USB storage will be read-only")
 storage.disable_usb_drive()
 print("USB drive disabled as not in safe mode")
 
-from ctrl import toggle_mouse_mover
-toggle_mouse_mover(False)
-print("Mouse mover toggled")
+if os.getenv("TOGGLE_ON_RESET"):
+    from ctrl import toggle_mouse_mover
+    toggle_mouse_mover(False)
+    print("Mouse mover toggled")
