c8650515d50eb15fa34b7a20dc6c83179244d0a1
Mateusz Drab <mateuszd@mpd.pw>
2024-12-18 02:18:01 +0000

    Add LED blink functionality to mouse mover

--- code.py
+++ code.py
@@ -3,12 +3,20 @@ import usb_hid
 from adafruit_hid.mouse import Mouse
 import os
 
+# For LED control
+import board
+import pwmio
+
+led = pwmio.PWMOut(board.GP22, frequency=5000, duty_cycle=65535)
+
 mouse = Mouse(usb_hid.devices)
 
 wait = os.getenv("MOVE_INTERVAL") / 1000
 move_distance = os.getenv("MOVE_STEP")
 mouse_moving = os.getenv("MOVE_MOVING")
 state = 0
+
+blink_interval = 0.05
 
 print("Mouse mover started")
 
@@ -26,9 +34,13 @@ while mouse_moving:
         x, y = -move_distance, move_distance
         move_name = "UP_LEFT"
 
+    led.duty_cycle = 63000
     mouse.move(x=x, y=y)
     print(f"Mouse did {move_name} with coordinates ({x}, {y})")
-    time.sleep(wait)
+    time.sleep(blink_interval)
+    led.duty_cycle = 65000
+
+    time.sleep(wait - blink_interval)
 
     state = (state + 1) % 4
 
